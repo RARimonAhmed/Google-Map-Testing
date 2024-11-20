2779d004097352f1d163c0d132c61b1fd97a1a59
RARimonAhmed <rayhanahmedrimon44@gmail.com>
2024-11-20 18:18:47 +0600

    Location permission is added

--- testing_flutter_google_map/lib/screens/map_screen.dart
+++ testing_flutter_google_map/lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -9,13 +10,42 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-
   LatLng pGooglePlex = const LatLng(37.4223, -122.0848);
+  LatLng pApplePark = const LatLng(37.3346, -122.0090);
+  Location location = Location();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(initialCameraPosition: CameraPosition(target: pGooglePlex,zoom: 13,)),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: pGooglePlex,
+          zoom: 13,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId('currentLocation'),
+            icon: BitmapDescriptor.defaultMarker,
+            position: pGooglePlex,
+          ),
+          Marker(
+            markerId: const MarkerId('sourceLocation'),
+            icon: BitmapDescriptor.defaultMarker,
+            position: pApplePark,
+          )
+        },
+      ),
     );
+  }
+
+  Future<void> getLocationUpdates() async {
+    bool serviceEnable;
+    PermissionStatus permissionStatus;
+    serviceEnable = await location.serviceEnabled();
+    if(serviceEnable){
+      await location.requestPermission();
+    }else{
+      return;
+    }
   }
 }
